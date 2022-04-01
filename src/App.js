743ca0f79eacfcf0e2c6af13688bda72ee74a6e3
Imon <contact@imonweb.co.uk>
2022-04-01 10:42:52 +0100

    Starter Files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
-       <h1>Hello World!</h1>
+       <h1>React JS</h1>
+       <p>
+         Hello {handleNameChange()}
+       </p>
     </div>
   );
 }
