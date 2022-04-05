14f23fa9fe5c93259e6132562423b0b3d0346e63
Imon <contact@imonweb.co.uk>
2022-04-05 14:05:50 +0100

    crud

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Content from './Content';
 import Footer from './Footer';
 import {useState, useEffect} from 'react'
 import SearchItems from './SearchItems';
+import apiRequest from './apiRequest';
 
 function App() {
     const API_URL = 'http://localhost:3500/items'
@@ -41,27 +42,54 @@ function App() {
     }, [])
   
 
-    const addItem = (item) => {
+    const addItem = async (item) => {
       const id = items.length ? items[items.length - 1].id + 1 : 1;
       const myNewItem = { id, checked: false, item };
       const listItems = [...items, myNewItem];
       setItems(listItems);
 
+      const postOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(myNewItem)
+      }
+      const result = await apiRequest(API_URL, postOptions)
+      if(result) setFetchError(result)
+
     }
 
-    const handleCheck = (id) => {
+    const handleCheck = async (id) => {
       // console.log(`key: ${id}`)
       const listItems = items.map((item) => item.id === id ? { ...item, 
       checked: !item.checked } : item);
       setItems(listItems);
+
+      const myItem = listItems.filter((item) => item.id === id)
+      const updateOptions = {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({checked: myItem[0].checked})
+      }
+      const reqUrl = `${API_URL}/${id}`;
+      const result = await apiRequest(reqUrl, updateOptions)
+      if(result) setFetchError(result)
     }
 
-    const handleDelete = (id) => {
+    const handleDelete = async (id) => {
       // console.log(id)
       const listItems = items.filter((item) => item.id !== id);
       // setItems(listItems);
       // localStorage.setItem('shoppinglist', JSON.stringify(listItems));
       setItems(listItems);
+
+      const deleteOptions = { method: 'DELETE' }
+      const reqUrl = `${API_URL}/${id}`;
+       const result = await apiRequest(reqUrl, deleteOptions)
+      if(result) setFetchError(result)
     }
 
     const handleSubmit = (e) => {
